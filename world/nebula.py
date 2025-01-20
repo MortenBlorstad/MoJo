431abc6ccc6c1da8d93ebef804c6fa6c3666143c
mortenblorstad <mblorstad@gmail.com>
2025-01-20 12:49:28 +0100

    added random params possibilities to abstract class

--- world/nebula.py
+++ world/nebula.py
@@ -8,7 +8,10 @@ class Nebula(base_component):
     def __init__(self, horizon):
         self.horizon = horizon
         self.map = jnp.zeros((1+horizon,24,24))
-        self.drift = None
+        self.nebula_tile_drift_speed = jnp.mean(self.nebula_tile_drift_speed_possibilities) # 0
+
+
+
         print("Nebula")
 
 
