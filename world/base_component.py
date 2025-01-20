862e262a1f61890e8fe91369fcae5bb90c74c938
mortenblorstad <mblorstad@gmail.com>
2025-01-20 13:01:48 +0100

    added random params possibilities to abstract class

--- world/base_component.py
+++ world/base_component.py
@@ -7,18 +7,18 @@ class base_component(ABC):
         super().__init__()
         self.env_params_ranges = dict(
             # map_type=[1],
-            unit_move_cost_possibilities =list(range(1, 6)),
-            unit_sensor_range_possibilities=list(range(2, 5)),
-            nebula_tile_vision_reduction_possibilities=list(range(0, 4)),
-            nebula_tile_energy_reduction_possibilities=[0, 0, 10, 25],
-            unit_sap_cost_possibilities=list(range(30, 51)),
-            unit_sap_range_possibilities=list(range(3, 8)),
-            unit_sap_dropoff_factor_possibilities=[0.25, 0.5, 1],
-            unit_energy_void_factor_possibilities=[0.0625, 0.125, 0.25, 0.375],
+            unit_move_cost=list(range(1, 6)),
+            unit_sensor_range=list(range(2, 5)),
+            nebula_tile_vision_reduction=list(range(0, 4)),
+            nebula_tile_energy_reduction=[0, 0, 10, 25],
+            unit_sap_cost=list(range(30, 51)),
+            unit_sap_range=list(range(3, 8)),
+            unit_sap_dropoff_factor=[0.25, 0.5, 1],
+            unit_energy_void_factor=[0.0625, 0.125, 0.25, 0.375],
             # map randomizations
-            nebula_tile_drift_speed_possibilities=[-0.05, -0.025, 0.025, 0.05],
-            energy_node_drift_speed_possibilities=[0.01, 0.02, 0.03, 0.04, 0.05],
-            energy_node_drift_magnitude_possibilities=list(range(3, 6)),
+            nebula_tile_drift_speed=[-0.05, -0.025, 0.025, 0.05],
+            energy_node_drift_speed=[0.01, 0.02, 0.03, 0.04, 0.05],
+            energy_node_drift_magnitude=list(range(3, 6)),
         )
 
 
--- world/nebula.py
+++ world/nebula.py
@@ -1,18 +1,18 @@
 from base_component import base_component
 import jax
 import jax.numpy as jnp
+import numpy as np
 
 
 class Nebula(base_component):
 
     def __init__(self, horizon):
+        super().__init__()
         self.horizon = horizon
         self.map = jnp.zeros((1+horizon,24,24))
-        self.nebula_tile_drift_speed = jnp.mean(self.nebula_tile_drift_speed_possibilities) # 0
-
-
-
-        print("Nebula")
+        self.nebula_tile_drift_speed = np.mean(self.env_params_ranges["nebula_tile_drift_speed"],dtype=np.float16) # 0
+        
+        print("Nebula", self.nebula_tile_drift_speed)
 
 
     def learn(self):
