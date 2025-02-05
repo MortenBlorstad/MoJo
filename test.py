92af839789012adcc203a7fc72ab0daf01ab3969
mortenblorstad <mblorstad@gmail.com>
2025-02-05 12:32:38 +0100

    things work now. Only need to implement the uncertainty prediction when we dont have observed drift_speed

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from world.obs_to_state import State
 import jax.numpy as jnp
 from world.utils import getObservation, getObsNamespace, getPath, from_json
 from world.nebula import Nebula
+from world.nebula_astroid import NebulaAstroid
 import flax
 from agents.agent import Agent
 
@@ -53,7 +54,7 @@ def pad_image(image, block_size=16):
     padded_image[:h, :w] = image
     return padded_image
 
-def plot_state_comparison(current_step,correct_states, predicted_states):
+def plot_state_comparison(current_step, correct_states, predicted_states, observable):
     """
     Plots the correct states in the first row and predicted states in the second row.
     
@@ -65,26 +66,46 @@ def plot_state_comparison(current_step,correct_states, predicted_states):
     plots_dir = "MoJo/plots"
     os.makedirs(plots_dir, exist_ok=True)
     fig, axes = plt.subplots(2, 4, figsize=(12, 6))
-    
+    observable = np.array(observable,dtype=float)
+    observable[observable==1]=np.inf
+    nebula_predictions, astroid_predictions = predicted_states
     for i in range(4):
-        correct_state = np.array(correct_states[i])
-        predicted_state = np.array(predicted_states[i])
+        correct_state = np.array(correct_states[i],dtype=float)
+        
+        
+
+        nebula_prediction = np.array(nebula_predictions[i],dtype=float)
+        astroid_prediction = np.array(astroid_predictions[i],dtype=float)
+        
+
                 
         # Identify false positives: places where prediction is 1 but correct state is 0
-        false_positive_indices = np.array(np.where((predicted_state == 1) & (correct_state == 0)))
-        false_negative_indices = np.array(np.where((predicted_state == 0) & (correct_state == 1)))
+        false_positive_indices_nebula = np.array(np.where((nebula_prediction == 1) & (correct_state[:,:,0] == 0)))
+        false_negative_indices_nebula = np.array(np.where((nebula_prediction == 0) & (correct_state[:,:,0] == 1)))
 
+        false_positive_indices_astroid = np.array(np.where((astroid_prediction == 1) & (correct_state[:,:,1] == 0)))
+        false_negative_indices_astroid = np.array(np.where((astroid_prediction == 0) & (correct_state[:,:,1] == 1)))
+
+        correct_state[correct_state == 0.0] = np.inf
+        nebula_prediction[nebula_prediction != 1.0] = np.inf
+        astroid_prediction[astroid_prediction == 0.0] = np.inf
 
         # Plot correct state (top row)
-        axes[0, i].imshow(correct_state, aspect="auto")
+        axes[0, i].imshow(1-correct_state[:,:,0], aspect="auto")
+        axes[0, i].imshow(1-correct_state[:,:,1], aspect="auto", cmap = "gray")
+        axes[0, i].imshow(correct_state[:,:,2], aspect="auto", cmap = "autumn")
         axes[0, i].set_title(f"Correct - t={current_step+i}")
         #axes[0, i].axis("off")
 
         # Plot predicted state (bottom row)
-        axes[1, i].imshow(predicted_state, aspect="auto")
+        axes[1, i].imshow(1-nebula_prediction, aspect="auto")
+        axes[1, i].imshow(1-astroid_prediction, aspect="auto", cmap="gray", vmin = 0, vmax=1)
+        axes[1, i].imshow(observable/2, aspect="auto", cmap="gray", alpha=0.25, vmin = 0, vmax=1)
         axes[1, i].set_title(f"Predicted - t={current_step+i}")
-        axes[1, i].scatter(false_positive_indices[1], false_positive_indices[0], color="red", marker="x", label="False Positive", s=20)
-        axes[1, i].scatter(false_negative_indices[1], false_negative_indices[0], color="blue", marker="s", label="False Negative", s=20)
+        axes[1, i].scatter(false_positive_indices_nebula[1], false_positive_indices_nebula[0], color="red", marker="x", label="False Positive", s=20)
+        axes[1, i].scatter(false_negative_indices_nebula[1], false_negative_indices_nebula[0], color="blue", marker="s", label="False Negative", s=20)
+        axes[1, i].scatter(false_positive_indices_astroid[1], false_positive_indices_astroid[0], color="red", marker="x", label="False Positive", s=20)
+        axes[1, i].scatter(false_negative_indices_astroid[1], false_negative_indices_astroid[0], color="blue", marker="s", label="False Negative", s=20)
         #axes[1, i].axis("off")
 
     plt.tight_layout()
@@ -92,7 +113,7 @@ def plot_state_comparison(current_step,correct_states, predicted_states):
     plt.savefig(plot_filename)
     plt.close(fig) 
 
-def create_gif():
+def create_gif(seed):
     """
     Combines all saved plots into a GIF and deletes the individual plot images.
     """
@@ -101,7 +122,7 @@ def create_gif():
     os.makedirs(gif_dir, exist_ok=True)
 
     #gif_filename = os.path.join(gif_dir, "state_comparison.gif")
-    video_filename = os.path.join(gif_dir, "state_comparison.mp4")
+    video_filename = os.path.join(gif_dir, f"state_comparison_{seed}.mp4")
 
     # Get list of all plots
     image_files = sorted(glob.glob(os.path.join(plots_dir, "plot_*.png")))
@@ -134,14 +155,20 @@ def create_gif():
 
 
 env = LuxAIS3GymEnv() 
-seed = 1#0 #223344 #
+#seed 0: -0.05 ok
+#seed 1: -0.1 ok
+#seed 2: 0.1 ok
+#seed 3: -0.1
+#seed 223344: -0.15 ok
+seed = 223344 #223344 #2#
 obs, info = env.reset(seed=seed)
 actions = {
         "player_0": jnp.ones((16, 3), dtype=int),
         "player_1": jnp.ones((16, 3), dtype=int)
         }
 #env.env_params["nebula_tile_drift_speed"] = -0.05
-nebula = Nebula(3)
+#nebula = Nebula(3)
+nebula =  NebulaAstroid(3)
 #print(info)
 
 env_cfg = {"max_units":16, "map_width":24, "map_height": 24}
@@ -149,7 +176,8 @@ agent = Agent("player_1",env_cfg)
 obs = flax.serialization.to_state_dict(obs)["player_1"] 
 print(env.env_params.nebula_tile_drift_speed)
 episode = {}
-for step in range(1,100):
+n_steps = 100
+for step in range(1,n_steps):
     actions["player_1"] = agent.act(step,obs)
     obs, _, _, _, info,state = env.step(actions)
     obs = flax.serialization.to_state_dict(obs)["player_1"] 
@@ -161,56 +189,64 @@ for step in range(1,100):
     episode[step]["obs"] = my_state
 
 
-for step in range(1,96):
+for step in range(1,n_steps-4):
     obs = episode[step]["obs"]
-    nebulas = jnp.array(obs.nebulas.copy())
+
+    nebulas =  jnp.array(obs.nebulas.copy(), ) #jnp.array(obs.asteroids.copy()) #
+    astroids = jnp.array(obs.asteroids.copy())
     observable = jnp.array(obs.observeable_tiles.copy())
-    
-    nebula.learn(nebulas,observable,step-1)
-    predictions = nebula.predict(nebulas,observable,step-1)
-    prediction = predictions[0]
-    solution = episode[step]["state"].nebulas
-    solutions = [episode[step+i]["state"].nebulas for i in range(4)]
-
-
     print(step)
-    plot_state_comparison(step, solutions, predictions)
-    print("current", check_prediction_accuracy(solution, prediction))
+    nebula.learn(nebulas,astroids,observable,step)
+    predictions = nebula.predict(nebulas,astroids,observable,step)
+    
+    solution = jnp.stack([episode[step]["state"].nebulas,episode[step]["state"].asteroids,
+                           jnp.clip(episode[step]["state"].player_units_count, a_min =0, a_max = 1)],axis=-1)
+    solutions = [jnp.stack([episode[step+i]["state"].nebulas,
+                            episode[step+i]["state"].asteroids,
+                             jnp.clip(episode[step+i]["state"].player_units_count, a_min =0, a_max = 1)],
+                             axis=-1) for i in range(4)]
+    
+
+
+
+    
+    plot_state_comparison(step, solutions, predictions,observable)
+    #print("current", check_prediction_accuracy(solution, prediction))
     # if not check_prediction_accuracy(solution, prediction):
     #     print(solution.T)
     #     print(prediction.T)
-    print("\n")
-    prediction = predictions[1]
-    solution = episode[step+1]["state"].nebulas
+    # print("\n")
+    # prediction = predictions[1]
+    # solution = episode[step+1]["state"].nebulas
     
-    print("next prediction", check_prediction_accuracy(solution, prediction))
+    # print("next prediction", check_prediction_accuracy(solution, prediction))
     # if not check_prediction_accuracy(solution, prediction):
     #     print(solution.T)
     #     print(prediction.T)
 
     
-    print("\n")
-    prediction = predictions[2]
-    solution = episode[step+2]["state"].nebulas
+    # print("\n")
+    # prediction = predictions[2]
+    # solution = episode[step+2]["state"].nebulas
     
-    print("next next prediction", check_prediction_accuracy(solution, prediction))
+    #print("next next prediction", check_prediction_accuracy(solution, prediction))
     # if not check_prediction_accuracy(solution, prediction):
     #     print(solution.T)
     #     print(prediction.T)
     
-    print("\n")
-    prediction = predictions[3]
-    solution = episode[step+3]["state"].nebulas
+    # print("\n")
+    # prediction = predictions[3]
+    # solution = episode[step+3]["state"].nebulas
     
-    print("next next next prediction", check_prediction_accuracy(solution, prediction))
+    # print("next next next prediction", check_prediction_accuracy(solution, prediction))
     # if not check_prediction_accuracy(solution, prediction):
     #     print(solution.T)
     #     print(prediction.T)
-    print("=====\n")
+    #print("=====\n")
     
 #create_gif()
 
-create_gif()
+create_gif(seed)
     
  
 
