2fc62e2086ca5e9eb7433d13c156e1418cce4ead
mortenblorstad <mblorstad@gmail.com>
2025-03-03 13:22:02 +0100

    added file

--- trainer/replay_memory.py
+++ trainer/replay_memory.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from collections import namedtuple
+from collections import namedtuple,deque
 
 # Define a named tuple for storing transitions
 Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'done'))
@@ -40,6 +40,53 @@ class ReplayMemory:
             list of Transition: A batch of sampled transitions.
         """
         return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+
+class SequenceReplayMemory:
+    def __init__(self, capacity, sequence_length):
+        """
+        Initialize the replay memory for sequences.
+        Args:
+            capacity (int): Maximum number of transitions to store.
+            sequence_length (int): Number of consecutive transitions per sample.
+        """
+        self.capacity = capacity
+        self.memory = deque(maxlen=capacity)  # Circular buffer using deque
+        self.sequence_length = sequence_length
+
+    def push(self, state, action, reward, next_state, done):
+        """
+        Store a transition in memory.
+        Args:
+            state (np.ndarray): Current state.
+            action (int): Action taken.
+            reward (float): Reward received.
+            next_state (np.ndarray): Next state.
+            done (bool): Whether the episode terminated.
+        """
+        self.memory.append(Transition(state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        """
+        Sample a batch of sequences from memory.
+        Args:
+            batch_size (int): Number of sequences to sample.
+        Returns:
+            list of list of Transition: A batch of sampled sequences.
+        """
+        if len(self.memory) < self.sequence_length:
+            return []
+
+        sequences = []
+        for _ in range(batch_size):
+            start_index = random.randint(0, len(self.memory) - self.sequence_length)
+            sequence = list(self.memory)[start_index:start_index + self.sequence_length]
+            sequences.append(sequence)
+
+        return sequences
 
     def __len__(self):
         return len(self.memory)
