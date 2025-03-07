f400382f71c9b175bcaef8a906ee26c584fe9853
Jørgen Mjaaseth <jorgen@mjaaseth.no>
2025-03-07 12:20:31 +0100

    Updated wandbwrapper

--- hierarchical/wandbwrapper.py
+++ hierarchical/wandbwrapper.py
@@ -4,10 +4,10 @@ class WandbWrapper():
 
     def __init__(self, usewandb):
 
-        self.usewandb = usewandb
+        self._WW_usewandb = usewandb
 
-        if self.usewandb:
-
+        if self._WW_usewandb:
+            
             # start a new wandb run to track this script
             wandb.init(
                 # set the wandb project where this run will be logged
@@ -18,11 +18,12 @@ class WandbWrapper():
                 #    "some_param": "has_been_set"
                 #}
             )
-            self.run = wandb.init()        
+            self._WW_run = wandb.init()
+
 
     #Record loss value
     def record(self, propertyName, propertyValue):
-        if self.usewandb:
+        if self._WW_usewandb:
             x = getattr(self, propertyName)
             if isinstance(x,list):
                 x.append(propertyValue)
@@ -33,7 +34,7 @@ class WandbWrapper():
     #Clear everything
     def __clear(self):
         for (k, v) in self.__dict__.items():
-            if(k == "run" or k == "usewandb"):
+            if(k[0:4] == "_WW_"):
                 pass
             else:
                 if isinstance(v, list):
@@ -42,30 +43,34 @@ class WandbWrapper():
                     setattr(self, k, 0)
 
     def report(self):
-        if self.usewandb:
+        if self._WW_usewandb:
             r = {}
             for (k, v) in self.__dict__.items():
-                if(k == "run" or k == "usewandb"):
+                if(k[0:4] == "_WW_"):
                     pass
                 else:
                     if isinstance(v, list):
                         if len(v) == 0:
-                            r[k] = 0
+                            r[k] = 0                    
                         else:
                             r[k] = sum(v) / len(v)
+                    elif isinstance(v, dict):
+                        r.update(v)
                     else:
                         r[k] = v
 
             self.__clear()        
-            self.run.log(r)        
-
-#Example
+            self._WW_run.log(r)
+            
 '''
-ww = WandbWrapper()
+#Example
+
+ww = WandbWrapper(True)
 ww.wmloss = 0
 ww.goalloss = 0
 ww.mgrloss = []
 ww.wrkloss = []
+ww.mstuff = 0
 
 
 for i in range(3):
@@ -76,4 +81,15 @@ for i in range(3):
     ww.record("mgrloss",-1)
     ww.record("mgrloss",-2)
     ww.record("mgrloss",-3)
+
+
+    d = {
+        "val1" : i,
+        "val2" : i+5
+    }
+    ww.record("mstuff",d)
+
+
+    ww.report()
+
 '''
